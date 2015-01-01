30c8a746669e59b0d80449c12f7cee807bf59d1c
Helmut Hummel <helmut.hummel@typo3.org>
2015-01-01 19:23:39 +0100

    Reformat ext_emconf.php to use tabs

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,47 +9,40 @@
 *********************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-  'title' => 'RealURL: speaking paths for TYPO3',
-  'description' => 'Creates nice looking URLs for TYPO3 pages: converts http://example.com/index.phpid=12345&L=2 to http://example.com/path/to/your/page/. Please, ask for free support in TYPO3 mailing lists or contact the maintainer for paid support.',
-  'category' => 'fe',
-  'shy' => 0,
-  'dependencies' => '',
-  'conflicts' => 'cooluri,simulatestatic',
-  'priority' => '',
-  'loadOrder' => '',
-  'module' => '',
-  'state' => 'alpha',
-  'internal' => 0,
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'modify_tables' => 'pages,sys_domain,pages_language_overlay,sys_template',
-  'clearCacheOnLoad' => 1,
-  'lockType' => '',
-  'author' => 'Dmitry Dulepov',
-  'author_email' => 'dmitry.dulepov@gmail.com',
-  'author_company' => '',
-  'CGLcompliance' => '',
-  'CGLcompliance_note' => '',
-  'version' => '1.12.8',
-  '_md5_values_when_last_written' => '',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'php' => '5.3.7-5.5.999',
-      'typo3' => '6.2.6-7.99.99',
-    ),
-    'conflicts' => 
-    array (
-      'cooluri' => '',
-      'simulatestatic' => '',
-    ),
-    'suggests' => 
-    array (
-      'static_info_tables' => '2.0.2-',
-    ),
-  ),
-  'suggests' => 
-  array (
-  ),
+	'title' => 'RealURL: speaking paths for TYPO3',
+	'description' => 'Creates nice looking URLs for TYPO3 pages: converts http://example.com/index.phpid=12345&L=2 to http://example.com/path/to/your/page/. Please, ask for free support in TYPO3 mailing lists or contact the maintainer for paid support.',
+	'category' => 'fe',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => 'cooluri,simulatestatic',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => 0,
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'pages,sys_domain,pages_language_overlay,sys_template',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Dmitry Dulepov',
+	'author_email' => 'dmitry.dulepov@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '1.12.8',
+	'_md5_values_when_last_written' => '',
+	'constraints' => array (
+		'depends' => array (
+			'php' => '5.3.7-5.5.999',
+			'typo3' => '6.2.6-7.99.99',
+		),
+		'conflicts' => array (
+			'cooluri' => '',
+			'simulatestatic' => '',
+		),
+		'suggests' => array (
+			'static_info_tables' => '2.0.2-',
+		),
+	),
 );
