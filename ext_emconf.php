17dac5299b6279dfc212a0139616da27da462a84
Helmut Hummel <info@helhum.io>
2016-09-13 23:30:20 +0200

    [TASK] Allow PHP 7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'_md5_values_when_last_written' => '',
 	'constraints' => array (
 		'depends' => array (
-			'php' => '5.3.7-5.6.999',
+			'php' => '5.3.7-7.1.999',
 			'typo3' => '6.2.6-8.2.99',
 		),
 		'conflicts' => array (
